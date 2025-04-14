347562b11e0372a84c3631f56b571067b57d2298
zhuzexi <zhuzexi@tansun.com.cn>
2025-04-14 14:57:42 +0800

    修改插件模块样式

--- web/app/components/plugins/plugin-page/plugins-panel.tsx
+++ web/app/components/plugins/plugin-page/plugins-panel.tsx
@@ -1,57 +1,69 @@
-'use client'
-import { useMemo } from 'react'
-import type { FilterState } from './filter-management'
-import FilterManagement from './filter-management'
-import List from './list'
-import { useInstalledPluginList, useInvalidateInstalledPluginList } from '@/service/use-plugins'
-import PluginDetailPanel from '@/app/components/plugins/plugin-detail-panel'
-import { usePluginPageContext } from './context'
-import { useDebounceFn } from 'ahooks'
-import Empty from './empty'
-import Loading from '../../base/loading'
+"use client";
+import { useMemo } from "react";
+import type { FilterState } from "./filter-management";
+import FilterManagement from "./filter-management";
+import List from "./list";
+import {
+  useInstalledPluginList,
+  useInvalidateInstalledPluginList,
+} from "@/service/use-plugins";
+import PluginDetailPanel from "@/app/components/plugins/plugin-detail-panel";
+import { usePluginPageContext } from "./context";
+import { useDebounceFn } from "ahooks";
+import Empty from "./empty";
+import Loading from "../../base/loading";
 
 const PluginsPanel = () => {
-  const filters = usePluginPageContext(v => v.filters) as FilterState
-  const setFilters = usePluginPageContext(v => v.setFilters)
-  const { data: pluginList, isLoading: isPluginListLoading } = useInstalledPluginList()
-  const invalidateInstalledPluginList = useInvalidateInstalledPluginList()
-  const currentPluginID = usePluginPageContext(v => v.currentPluginID)
-  const setCurrentPluginID = usePluginPageContext(v => v.setCurrentPluginID)
+  const filters = usePluginPageContext((v) => v.filters) as FilterState;
+  const setFilters = usePluginPageContext((v) => v.setFilters);
+  const { data: pluginList, isLoading: isPluginListLoading } =
+    useInstalledPluginList();
+  const invalidateInstalledPluginList = useInvalidateInstalledPluginList();
+  const currentPluginID = usePluginPageContext((v) => v.currentPluginID);
+  const setCurrentPluginID = usePluginPageContext((v) => v.setCurrentPluginID);
 
-  const { run: handleFilterChange } = useDebounceFn((filters: FilterState) => {
-    setFilters(filters)
-  }, { wait: 500 })
+  const { run: handleFilterChange } = useDebounceFn(
+    (filters: FilterState) => {
+      setFilters(filters);
+    },
+    { wait: 500 }
+  );
 
   const filteredList = useMemo(() => {
-    const { categories, searchQuery, tags } = filters
+    const { categories, searchQuery, tags } = filters;
     const filteredList = pluginList?.plugins.filter((plugin) => {
       return (
-        (categories.length === 0 || categories.includes(plugin.declaration.category))
-        && (tags.length === 0 || tags.some(tag => plugin.declaration.tags.includes(tag)))
-        && (searchQuery === '' || plugin.plugin_id.toLowerCase().includes(searchQuery.toLowerCase()))
-      )
-    })
-    return filteredList
-  }, [pluginList, filters])
+        (categories.length === 0 ||
+          categories.includes(plugin.declaration.category)) &&
+        (tags.length === 0 ||
+          tags.some((tag) => plugin.declaration.tags.includes(tag))) &&
+        (searchQuery === "" ||
+          plugin.plugin_id.toLowerCase().includes(searchQuery.toLowerCase()))
+      );
+    });
+    return filteredList;
+  }, [pluginList, filters]);
 
   const currentPluginDetail = useMemo(() => {
-    const detail = pluginList?.plugins.find(plugin => plugin.plugin_id === currentPluginID)
-    return detail
-  }, [currentPluginID, pluginList?.plugins])
+    const detail = pluginList?.plugins.find(
+      (plugin) => plugin.plugin_id === currentPluginID
+    );
+    return detail;
+  }, [currentPluginID, pluginList?.plugins]);
 
-  const handleHide = () => setCurrentPluginID(undefined)
+  const handleHide = () => setCurrentPluginID(undefined);
 
   return (
-    <>
-      <div className='flex flex-col items-start justify-center gap-3 self-stretch px-12 pb-3 pt-1'>
-        <div className='h-px self-stretch bg-divider-subtle'></div>
-        <FilterManagement
-          onFilterChange={handleFilterChange}
-        />
+    <div className="bg-components-panel-bg rounded-b-lg">
+      <div className="flex flex-col items-start justify-center gap-3 self-stretch px-12 pb-3 pt-1">
+        <div className="h-px self-stretch bg-divider-subtle"></div>
+        <FilterManagement onFilterChange={handleFilterChange} />
       </div>
-      {isPluginListLoading ? <Loading type='app' /> : (filteredList?.length ?? 0) > 0 ? (
-        <div className='flex grow flex-wrap content-start items-start gap-2 self-stretch px-12'>
-          <div className='w-full'>
+      {isPluginListLoading ? (
+        <Loading type="app" />
+      ) : (filteredList?.length ?? 0) > 0 ? (
+        <div className="flex grow flex-wrap content-start items-start gap-2 self-stretch px-12">
+          <div className="w-full">
             <List pluginList={filteredList || []} />
           </div>
         </div>
@@ -63,8 +75,8 @@ const PluginsPanel = () => {
         onUpdate={() => invalidateInstalledPluginList()}
         onHide={handleHide}
       />
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default PluginsPanel
+export default PluginsPanel;
