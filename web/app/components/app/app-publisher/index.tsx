1e1e361b8fbe167ac6ce3e935e28c2e009033c87
zhuzexi <zhuzexi@tansun.com.cn>
2025-04-16 16:20:29 +0800

    chat发布运行时，appURL更换为浏览器地址

--- web/app/components/app/app-publisher/index.tsx
+++ web/app/components/app/app-publisher/index.tsx
@@ -1,8 +1,4 @@
-import {
-  memo,
-  useCallback,
-  useState,
-} from 'react'
+import { memo, useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import dayjs from 'dayjs'
 import {
@@ -35,21 +31,21 @@ import { appDefaultIconBackground } from '@/config'
 import type { PublishWorkflowParams } from '@/types/workflow'
 
 export type AppPublisherProps = {
-  disabled?: boolean
-  publishDisabled?: boolean
-  publishedAt?: number
+  disabled?: boolean;
+  publishDisabled?: boolean;
+  publishedAt?: number;
   /** only needed in workflow / chatflow mode */
-  draftUpdatedAt?: number
-  debugWithMultipleModel?: boolean
-  multipleModelConfigs?: ModelAndParameter[]
+  draftUpdatedAt?: number;
+  debugWithMultipleModel?: boolean;
+  multipleModelConfigs?: ModelAndParameter[];
   /** modelAndParameter is passed when debugWithMultipleModel is true */
-  onPublish?: (params?: any) => Promise<any> | any
-  onRestore?: () => Promise<any> | any
-  onToggle?: (state: boolean) => void
-  crossAxisOffset?: number
-  toolPublished?: boolean
-  inputs?: InputVar[]
-  onRefreshData?: () => void
+  onPublish?: (params?: any) => Promise<any> | any;
+  onRestore?: () => Promise<any> | any;
+  onToggle?: (state: boolean) => void;
+  crossAxisOffset?: number;
+  toolPublished?: boolean;
+  inputs?: InputVar[];
+  onRefreshData?: () => void;
 }
 
 const PUBLISH_SHORTCUT = ['⌘', '⇧', 'P']
@@ -73,32 +69,59 @@ const AppPublisher = ({
   const [published, setPublished] = useState(false)
   const [open, setOpen] = useState(false)
   const appDetail = useAppStore(state => state.appDetail)
-  const { app_base_url: appBaseURL = '', access_token: accessToken = '' } = appDetail?.site ?? {}
-  const appMode = (appDetail?.mode !== 'completion' && appDetail?.mode !== 'workflow') ? 'chat' : appDetail.mode
-  const appURL = `${appBaseURL}/${appMode}/${accessToken}`
-  const isChatApp = ['chat', 'agent-chat', 'completion'].includes(appDetail?.mode || '')
+  const { app_base_url: appBaseURL = '', access_token: accessToken = '' }
+    = appDetail?.site ?? {}
+  const appMode
+    = appDetail?.mode !== 'completion' && appDetail?.mode !== 'workflow'
+      ? 'chat'
+      : appDetail.mode
+
+  let appURL_location = ''
+  // 判断是开发还是生产
+  console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+  if (process.env.NODE_ENV === 'development') {
+    appURL_location
+      = process.env.NEXT_PUBLIC_PUBLIC_API_PREFIX?.replace('/api', '') || ''
+  }
+ else {
+    appURL_location = window.location.origin
+  }
+  // const appURL = `${appBaseURL}/${appMode}/${accessToken}`
+  const appURL = `${appURL_location}/${appMode}/${accessToken}`
+
+  const isChatApp = ['chat', 'agent-chat', 'completion'].includes(
+    appDetail?.mode || '',
+  )
 
   const language = useGetLanguage()
-  const formatTimeFromNow = useCallback((time: number) => {
-    return dayjs(time).locale(language === 'zh_Hans' ? 'zh-cn' : language.replace('_', '-')).fromNow()
-  }, [language])
+  const formatTimeFromNow = useCallback(
+    (time: number) => {
+      return dayjs(time)
+        .locale(language === 'zh_Hans' ? 'zh-cn' : language.replace('_', '-'))
+        .fromNow()
+    },
+    [language],
+  )
 
-  const handlePublish = useCallback(async (params?: ModelAndParameter | PublishWorkflowParams) => {
-    try {
-      await onPublish?.(params)
-      setPublished(true)
-    }
-    catch {
-      setPublished(false)
-    }
-  }, [onPublish])
+  const handlePublish = useCallback(
+    async (params?: ModelAndParameter | PublishWorkflowParams) => {
+      try {
+        await onPublish?.(params)
+        setPublished(true)
+      }
+ catch {
+        setPublished(false)
+      }
+    },
+    [onPublish],
+  )
 
   const handleRestore = useCallback(async () => {
     try {
       await onRestore?.()
       setOpen(false)
     }
-    catch {}
+ catch {}
   }, [onRestore])
 
   const handleTrigger = useCallback(() => {
@@ -112,159 +135,159 @@ const AppPublisher = ({
     onToggle?.(state)
     setOpen(state)
 
-    if (state)
-      setPublished(false)
+    if (state) setPublished(false)
   }, [disabled, onToggle, open])
 
   const handleOpenInExplore = useCallback(async () => {
     try {
-      const { installed_apps }: any = await fetchInstalledAppList(appDetail?.id) || {}
+      const { installed_apps }: any
+        = (await fetchInstalledAppList(appDetail?.id)) || {}
       if (installed_apps?.length > 0)
         window.open(`/explore/installed/${installed_apps[0].id}`, '_blank')
-      else
-        throw new Error('No app found in Explore')
+      else throw new Error('No app found in Explore')
     }
-    catch (e: any) {
+ catch (e: any) {
       Toast.notify({ type: 'error', message: `${e.message || e}` })
     }
   }, [appDetail?.id])
 
   const [embeddingModalOpen, setEmbeddingModalOpen] = useState(false)
 
-  useKeyPress(`${getKeyboardKeyCodeBySystem('ctrl')}.shift.p`, (e) => {
-    e.preventDefault()
-    if (publishDisabled || published)
-      return
-    handlePublish()
-  },
-  { exactMatch: true, useCapture: true })
+  useKeyPress(
+    `${getKeyboardKeyCodeBySystem('ctrl')}.shift.p`,
+    (e) => {
+      e.preventDefault()
+      if (publishDisabled || published) return
+      handlePublish()
+    },
+    { exactMatch: true, useCapture: true },
+  )
 
   return (
     <>
       <PortalToFollowElem
         open={open}
         onOpenChange={setOpen}
-        placement='bottom-end'
+        placement="bottom-end"
         offset={{
           mainAxis: 4,
           crossAxis: crossAxisOffset,
         }}
       >
         <PortalToFollowElemTrigger onClick={handleTrigger}>
-          <Button
-            variant='primary'
-            className='p-2'
-            disabled={disabled}
-          >
+          <Button variant="primary" className="p-2" disabled={disabled}>
             {t('workflow.common.publish')}
-            <RiArrowDownSLine className='h-4 w-4 text-components-button-primary-text' />
+            <RiArrowDownSLine className="h-4 w-4 text-components-button-primary-text" />
           </Button>
         </PortalToFollowElemTrigger>
-        <PortalToFollowElemContent className='z-[11]'>
-          <div className='w-[320px] rounded-2xl border-[0.5px] border-components-panel-border bg-components-panel-bg shadow-xl shadow-shadow-shadow-5'>
-            <div className='p-4 pt-3'>
-              <div className='system-xs-medium-uppercase flex h-6 items-center text-text-tertiary'>
-                {publishedAt ? t('workflow.common.latestPublished') : t('workflow.common.currentDraftUnpublished')}
+        <PortalToFollowElemContent className="z-[11]">
+          <div className="w-[320px] rounded-2xl border-[0.5px] border-components-panel-border bg-components-panel-bg shadow-xl shadow-shadow-shadow-5">
+            <div className="p-4 pt-3">
+              <div className="system-xs-medium-uppercase flex h-6 items-center text-text-tertiary">
+                {publishedAt
+                  ? t('workflow.common.latestPublished')
+                  : t('workflow.common.currentDraftUnpublished')}
               </div>
-              {publishedAt
-                ? (
-                  <div className='flex items-center justify-between'>
-                    <div className='system-sm-medium flex items-center text-text-secondary'>
-                      {t('workflow.common.publishedAt')} {formatTimeFromNow(publishedAt)}
-                    </div>
-                    {isChatApp && <Button
-                      variant='secondary-accent'
-                      size='small'
+              {publishedAt ? (
+                <div className="flex items-center justify-between">
+                  <div className="system-sm-medium flex items-center text-text-secondary">
+                    {t('workflow.common.publishedAt')}{' '}
+                    {formatTimeFromNow(publishedAt)}
+                  </div>
+                  {isChatApp && (
+                    <Button
+                      variant="secondary-accent"
+                      size="small"
                       onClick={handleRestore}
                       disabled={published}
                     >
                       {t('workflow.common.restore')}
-                    </Button>}
-                  </div>
-                )
-                : (
-                  <div className='system-sm-medium flex items-center text-text-secondary'>
-                    {t('workflow.common.autoSaved')} · {Boolean(draftUpdatedAt) && formatTimeFromNow(draftUpdatedAt!)}
-                  </div>
-                )}
-              {debugWithMultipleModel
-                ? (
-                  <PublishWithMultipleModel
-                    multipleModelConfigs={multipleModelConfigs}
-                    onSelect={item => handlePublish(item)}
+                    </Button>
+                  )}
+                </div>
+              ) : (
+                <div className="system-sm-medium flex items-center text-text-secondary">
+                  {t('workflow.common.autoSaved')} ·{' '}
+                  {Boolean(draftUpdatedAt)
+                    && formatTimeFromNow(draftUpdatedAt!)}
+                </div>
+              )}
+              {debugWithMultipleModel ? (
+                <PublishWithMultipleModel
+                  multipleModelConfigs={multipleModelConfigs}
+                  onSelect={item => handlePublish(item)}
                   // textGenerationModelList={textGenerationModelList}
-                  />
-                )
-                : (
-                  <Button
-                    variant='primary'
-                    className='mt-3 w-full'
-                    onClick={() => handlePublish()}
-                    disabled={publishDisabled || published}
-                  >
-                    {
-                      published
-                        ? t('workflow.common.published')
-                        : (
-                          <div className='flex gap-1'>
-                            <span>{t('workflow.common.publishUpdate')}</span>
-                            <div className='flex gap-0.5'>
-                              {PUBLISH_SHORTCUT.map(key => (
-                                <span key={key} className='system-kbd h-4 w-4 rounded-[4px] bg-components-kbd-bg-white text-text-primary-on-surface'>
-                                  {key}
-                                </span>
-                              ))}
-                            </div>
-                          </div>
-                        )
-                    }
-                  </Button>
-                )
-              }
+                />
+              ) : (
+                <Button
+                  variant="primary"
+                  className="mt-3 w-full"
+                  onClick={() => handlePublish()}
+                  disabled={publishDisabled || published}
+                >
+                  {published ? (
+                    t('workflow.common.published')
+                  ) : (
+                    <div className="flex gap-1">
+                      <span>{t('workflow.common.publishUpdate')}</span>
+                      <div className="flex gap-0.5">
+                        {PUBLISH_SHORTCUT.map(key => (
+                          <span
+                            key={key}
+                            className="system-kbd h-4 w-4 rounded-[4px] bg-components-kbd-bg-white text-text-primary-on-surface"
+                          >
+                            {key}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                </Button>
+              )}
             </div>
-            <div className='border-t-[0.5px] border-t-divider-regular p-4 pt-3'>
+            <div className="border-t-[0.5px] border-t-divider-regular p-4 pt-3">
               <SuggestedAction
                 disabled={!publishedAt}
                 link={appURL}
-                icon={<RiPlayCircleLine className='h-4 w-4' />}
+                icon={<RiPlayCircleLine className="h-4 w-4" />}
               >
                 {t('workflow.common.runApp')}
               </SuggestedAction>
-              {appDetail?.mode === 'workflow'
-                ? (
-                  <SuggestedAction
-                    disabled={!publishedAt}
-                    link={`${appURL}${appURL.includes('?') ? '&' : '?'}mode=batch`}
-                    icon={<RiPlayList2Line className='h-4 w-4' />}
-                  >
-                    {t('workflow.common.batchRunApp')}
-                  </SuggestedAction>
-                )
-                : (
-                  <SuggestedAction
-                    onClick={() => {
-                      setEmbeddingModalOpen(true)
-                      handleTrigger()
-                    }}
-                    disabled={!publishedAt}
-                    icon={<CodeBrowser className='h-4 w-4' />}
-                  >
-                    {t('workflow.common.embedIntoSite')}
-                  </SuggestedAction>
-                )}
+              {appDetail?.mode === 'workflow' ? (
+                <SuggestedAction
+                  disabled={!publishedAt}
+                  link={`${appURL}${
+                    appURL.includes('?') ? '&' : '?'
+                  }mode=batch`}
+                  icon={<RiPlayList2Line className="h-4 w-4" />}
+                >
+                  {t('workflow.common.batchRunApp')}
+                </SuggestedAction>
+              ) : (
+                <SuggestedAction
+                  onClick={() => {
+                    setEmbeddingModalOpen(true)
+                    handleTrigger()
+                  }}
+                  disabled={!publishedAt}
+                  icon={<CodeBrowser className="h-4 w-4" />}
+                >
+                  {t('workflow.common.embedIntoSite')}
+                </SuggestedAction>
+              )}
               <SuggestedAction
                 onClick={() => {
                   publishedAt && handleOpenInExplore()
                 }}
                 disabled={!publishedAt}
-                icon={<RiPlanetLine className='h-4 w-4' />}
+                icon={<RiPlanetLine className="h-4 w-4" />}
               >
                 {t('workflow.common.openInExplore')}
               </SuggestedAction>
               <SuggestedAction
                 disabled={!publishedAt}
-                link='./develop'
-                icon={<RiTerminalBoxLine className='h-4 w-4' />}
+                link="./develop"
+                icon={<RiTerminalBoxLine className="h-4 w-4" />}
               >
                 {t('workflow.common.accessAPIReference')}
               </SuggestedAction>
@@ -275,8 +298,15 @@ const AppPublisher = ({
                   detailNeedUpdate={!!toolPublished && published}
                   workflowAppId={appDetail?.id}
                   icon={{
-                    content: (appDetail.icon_type === 'image' ? '🤖' : appDetail?.icon) || '🤖',
-                    background: (appDetail.icon_type === 'image' ? appDefaultIconBackground : appDetail?.icon_background) || appDefaultIconBackground,
+                    content:
+                      (appDetail.icon_type === 'image'
+                        ? '🤖'
+                        : appDetail?.icon) || '🤖',
+                    background:
+                      (appDetail.icon_type === 'image'
+                        ? appDefaultIconBackground
+                        : appDetail?.icon_background)
+                      || appDefaultIconBackground,
                   }}
                   name={appDetail?.name}
                   description={appDetail?.description}
@@ -295,7 +325,7 @@ const AppPublisher = ({
           appBaseUrl={appBaseURL}
           accessToken={accessToken}
         />
-      </PortalToFollowElem >
+      </PortalToFollowElem>
     </>
   )
 }
